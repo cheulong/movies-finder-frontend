c6a7444ec1d9a7920daf4563dd5c7f535d1b4f8a
Cheulong Sear <searcheulong@gmail.com>
2025-09-17 15:05:51 +0700

    chore: copy code from frontend folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import customLogger from "../logger";
-const logger=customLogger({ serviceName: 'react-app' })
 
 function App() {
-  const [count, setCount] = useState(0);
-  const handleClick = () => {
-    setCount((count) => count + 1);
-    // emit a log record
-    logger.info('info');
-    logger.warn('warn');
-    logger.error('error');
-    logger.fatal('fatal');
-    logger.trace('trace');
-    logger.debug('debug');
-    logger.notset('notset');
-  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={handleClick}>count is {count}</button>
-        <p>Branch change title</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-3xl font-bold underline">Movies Finder</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
